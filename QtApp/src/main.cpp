16c9866c335b55fd736898c9565f68ccf7d4885d
FateRider <faterider53@gmail.com>
2025-07-18 21:49:43 +0300

    Qt hmm Cmake HMMM FUCKİNG QT LİBS FUCKİNG QT DOCS FUCKİNG CMAKE senpaii ! mmm senpai ?

--- QtApp/src/main.cpp
+++ QtApp/src/main.cpp
@@ -10,16 +10,21 @@ int main(int argc, char *argv[]) {
     QWidget window;
     window.setWindowTitle("Hello Qt , I hate you");
     window.setWindowIcon(QIcon("../images/octopi_red.png"));
-    window.resize(900, 600);
+    window.setFixedSize(900, 600);
+
+    QLabel *image = new QLabel(&window);
+    image->setPixmap(QPixmap("../images/9SFjWW0.png"));
+    image->setGeometry(550, 20, 400, 600);
+    image->setScaledContents(true);
 
     QPushButton *btn = new QPushButton("Senpai pls click me UwU", &window);
-    btn->move(350, 300);
+    btn->move(500, 450);
 
     QPushButton *btn2 = new QPushButton("Restart", &window);
-    btn2->move(350, 340);
+    btn2->move(400, 450);
 
     QLabel *label = new QLabel("Not clicked button pls click senpai PLSSS NYAAAA OwO", &window);
-    label->move(350, 250);
+    label->move(300, 250);
 
     QObject::connect(btn, &QPushButton::clicked, [label]() { label->setText("UMMMHHH SENPAI CLICKED MEEE"); });
     QObject::connect(btn2, &QPushButton::clicked, [label]() { label->setText("Senpai pls again click me UwU"); });
